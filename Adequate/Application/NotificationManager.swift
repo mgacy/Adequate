6110c4b7fceb9da0a0ffe79fe5d4701c706ee0b1
mgacy <matt@mgacy.com>
2018-09-29 10:26:01 -0700

    Change notifications to exclude badges

--- Adequate/Application/NotificationManager.swift
+++ Adequate/Application/NotificationManager.swift
@@ -42,7 +42,7 @@ class NotificationManager: NSObject, NotificationManagerType {
 
     @discardableResult
     func registerForPushNotifications() -> Promise<Void> {
-        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        let options: UNAuthorizationOptions = [.alert, .sound]
         return notificationCenter.requestAuthorization(options: options)
             .ensure({ $0 })
             .then({ _ -> Promise<UNNotificationSettings> in
@@ -60,14 +60,15 @@ class NotificationManager: NSObject, NotificationManagerType {
 
 extension NotificationManager: UNUserNotificationCenterDelegate {
 
-    // Handle notifications when app is in foreground
+    // Called when a notification is delivered to a foreground app.
     func userNotificationCenter(_ center: UNUserNotificationCenter,
                                 willPresent notification: UNNotification,
                                 withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         // ...
-        completionHandler([.alert, .sound, .badge])
+        completionHandler([.alert, .sound])
     }
 
+    // Called to let your app know which action was selected by the user for a given notification.
     func userNotificationCenter(_ center: UNUserNotificationCenter,
                                 didReceive response: UNNotificationResponse,
                                 withCompletionHandler completionHandler: @escaping () -> Void) {
