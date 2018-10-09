230267b5b0d781465fee7d6260e110e88c84f2ae
mgacy <matt@mgacy.com>
2018-10-09 10:17:36 -0700

    Add FileDownloader, constants

---
+++ NotificationService/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  NotificationService
+//
+//  Created by Mathew Gacy on 10/9/18.
+//  Copyright © 2018 Mathew Gacy. All rights reserved.
+//
+
+//import Foundation
+
+struct NotificationConstants {
+    static let dealKey = "adequate-deal-url"
+    static let imageKey = "adequate-image-url"
+}
