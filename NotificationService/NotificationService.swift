574684a6ca91daff40e92e2d71f923a58d782386
mgacy <matt@mgacy.com>
2018-10-09 10:20:21 -0700

    Use FileDownloader

--- NotificationService/NotificationService.swift
+++ NotificationService/NotificationService.swift
@@ -12,15 +12,35 @@ class NotificationService: UNNotificationServiceExtension {
 
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
+    private let downloader = FileDownloader()
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
         
         if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
+
+            // Parse image url from bestAttemptContent?.userInfo
+            guard
+                let urlString = bestAttemptContent.userInfo[NotificationConstants.imageKey] as? String,
+                let url = URL(string: urlString)?.secure()
+                else {
+                    return contentHandler(request.content)
+            }
+
+            /// TODO: make NotificationConstants an enum and define attachmentID as property of it?
+            let attachmentID = "image"
+
+            // Download image and modify notification content
+            downloader.downloadFile(from: url, as: attachmentID) { url in
+                guard
+                    let url = url,
+                    let attachment = try? UNNotificationAttachment(identifier: attachmentID, url: url, options: nil)
+                    else {
+                        return
+                }
+                bestAttemptContent.attachments.append(attachment)
+            }            
             contentHandler(bestAttemptContent)
         }
     }
