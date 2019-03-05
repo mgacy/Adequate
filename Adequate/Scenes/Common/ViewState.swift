0836b58044ca19b0445cccdd068834dadad4361d
mgacy <matt@mgacy.com>
2019-03-04 21:05:39 -0700

    Add map() and Equatable conformance

--- Adequate/Scenes/Common/ViewState.swift
+++ Adequate/Scenes/Common/ViewState.swift
@@ -15,6 +15,39 @@ enum ViewState<Element> {
     case error(Error)
 }
 
+extension ViewState {
+    func map<T>(_ transform: (Element) -> T) -> ViewState<T> {
+        switch self {
+        case .empty:
+            return .empty
+        case .loading:
+            return .loading
+        case .result(let result):
+            return .result(transform(result))
+        case .error(let error):
+            return .error(error)
+        }
+    }
+}
+
+extension ViewState: Equatable where Element: Equatable {
+    static func == (lhs: ViewState<Element>, rhs: ViewState<Element>) -> Bool {
+        switch(lhs, rhs) {
+        case (.loading, .loading):
+            return true
+        case (.result(let a), .result(let b)):
+            return a == b
+        case (.empty , .empty):
+            return true
+        // See the following on adding Equatable conformance to Error:
+        // https://kandelvijaya.com/2018/04/21/blog_equalityonerror/
+        // case(.error(let a), .error(let b)):
+        default:
+            return false
+        }
+    }
+}
+
 protocol ViewStateRenderable: class {
     //associatedtype ResultType
     //func render()
