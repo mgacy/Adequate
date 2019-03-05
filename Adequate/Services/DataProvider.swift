def1907cab96ec548c1c485a028f80503f86b2ff
mgacy <matt@mgacy.com>
2019-03-04 21:13:21 -0700

    Add support for Deal history

--- Adequate/Services/DataProvider.swift
+++ Adequate/Services/DataProvider.swift
@@ -12,7 +12,9 @@ import Promise
 
 protocol DataProviderType {
     func getDeal()
+    func getDealHistory(from: Date, to: Date)
     func addDealObserver<T: AnyObject>(_: T, closure: @escaping (T, ViewState<Deal>) -> Void) -> ObservationToken
+    func addHistoryObserver<T: AnyObject>(_: T, closure: @escaping (T, ViewState<[Deal]>) -> Void) -> ObservationToken
 }
 
 // MARK: - Implementation
@@ -27,14 +29,22 @@ class DataProvider: DataProviderType {
         }
     }
 
+    private var historyState: ViewState<[Deal]> {
+        didSet {
+            callObservations(with: historyState)
+        }
+    }
+
     private let mehService: MehServiceType
     private var dealObservations: [UUID: (ViewState<Deal>) -> Void] = [:]
+    private var historyObservations: [UUID: (ViewState<[Deal]>) -> Void] = [:]
 
     // MARK: - Lifecycle
 
     init(mehService: MehServiceType) {
         self.mehService = mehService
         self.dealState = .empty
+        self.historyState = .empty
     }
 
     // MARK: - A
@@ -46,6 +56,16 @@ class DataProvider: DataProviderType {
             self.dealState = .result(response.deal)
         }).catch({ error in
             self.dealState = .error(error)
+        })
+    }
+
+    func getDealHistory(from startDate: Date, to endDate: Date) {
+        guard historyState != ViewState<[Deal]>.loading else { return }
+        historyState = .loading
+        mehService.getDeal().then({ response in
+            self.historyState = .result([response.deal, response.deal, response.deal, response.deal])
+        }).catch({ error in
+            self.historyState = .error(error)
         })
     }
 
@@ -70,8 +90,32 @@ class DataProvider: DataProviderType {
         }
     }
 
+    func addHistoryObserver<T: AnyObject>(_ observer: T, closure: @escaping (T, ViewState<[Deal]>) -> Void) -> ObservationToken {
+        let id = UUID()
+        historyObservations[id] = { [weak self, weak observer] state in
+            // If the observer has been deallocated, we can
+            // automatically remove the observation closure.
+            guard let observer = observer else {
+                self?.historyObservations.removeValue(forKey: id)
+                return
+            }
+            closure(observer, state)
+        }
+
+        closure(observer, historyState)
+        return ObservationToken { [weak self] in
+            self?.historyObservations.removeValue(forKey: id)
+        }
+    }
+
     private func callObservations(with dealState: ViewState<Deal>) {
         dealObservations.values.forEach { observation in
+            observation(dealState)
+        }
+    }
+
+    private func callObservations(with dealState: ViewState<[Deal]>) {
+        historyObservations.values.forEach { observation in
             observation(dealState)
         }
     }
