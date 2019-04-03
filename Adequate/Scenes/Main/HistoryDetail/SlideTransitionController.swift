a4e5ecc714427c3b6865914e8f4475a391522d75
mgacy <matt@mgacy.com>
2019-04-03 12:52:28 -0700

    Refactor animation controller

--- Adequate/Scenes/Main/HistoryDetail/SlideTransitionController.swift
+++ Adequate/Scenes/Main/HistoryDetail/SlideTransitionController.swift
@@ -89,11 +89,11 @@ extension SlideTransitionController: UIGestureRecognizerDelegate {
 extension SlideTransitionController: UIViewControllerTransitioningDelegate {
 
     func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        return PanelAnimationController(transitionType: .presenting)
+        return SlideAnimationController(transitionType: .presenting)
     }
 
     func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        return PanelAnimationController(transitionType: .dismissing)
+        return SlideAnimationController(transitionType: .dismissing)
     }
 
     func interactionControllerForPresentation(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
@@ -106,81 +106,6 @@ extension SlideTransitionController: UIViewControllerTransitioningDelegate {
 
     func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
         return DimmingPresentationController(presentedViewController: presented, presenting: presenting)
-    }
-
-}
-
-// MARK: - B
-
-class PanelAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
-
-    enum TransitionType {
-        case presenting
-        case dismissing
-    }
-
-    let transitionType: TransitionType
-
-    init(transitionType: TransitionType) {
-        self.transitionType = transitionType
-        super.init()
-    }
-
-    // MARK: - UIViewControllerAnimatedTransitioning
-
-    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        return 0.3
-    }
-
-    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-        guard
-            let fromVC = transitionContext.viewController(forKey: .from),
-            let toVC = transitionContext.viewController(forKey: .to) else {
-                return
-        }
-        switch transitionType {
-        case .presenting:
-            animatePresentation(from: fromVC, to: toVC, using: transitionContext)
-        case .dismissing:
-            animateDismissal(from: fromVC, to: toVC, using: transitionContext)
-        }
-    }
-
-    // MARK: - Present / Dismiss
-
-    private func animatePresentation(from fromVC: UIViewController, to toVC: UIViewController, using transitionContext: UIViewControllerContextTransitioning) {
-        let containerView = transitionContext.containerView
-        // TODO
-        let dy = containerView.frame.size.height
-        let finalFrame = transitionContext.finalFrame(for: toVC)
-
-        //log.debug("\(#function): \(fromVC) -> \(toVC) in \(containerView)")
-
-        toVC.view.frame = finalFrame.offsetBy(dx: 0.0, dy: dy)
-        containerView.addSubview(toVC.view)
-
-        UIView.animate(
-            withDuration: transitionDuration(using: transitionContext), delay: 0,
-            options: [ UIView.AnimationOptions.curveEaseOut ],
-            animations: {
-                toVC.view.frame = finalFrame
-        },
-            completion: { _ in transitionContext.completeTransition(!transitionContext.transitionWasCancelled) }
-        )
-    }
-
-    private func animateDismissal(from fromVC: UIViewController, to toVC: UIViewController, using transitionContext: UIViewControllerContextTransitioning) {
-        let containerView = transitionContext.containerView
-        let dy = containerView.frame.size.height
-        let initialFrame = fromVC.view.frame
-
-        UIView.animate(
-            withDuration: transitionDuration(using: transitionContext),
-            animations: {
-                fromVC.view.frame = initialFrame.offsetBy(dx: 0.0, dy: dy)
-            },
-            completion: { _ in transitionContext.completeTransition(!transitionContext.transitionWasCancelled) }
-        )
     }
 
 }
