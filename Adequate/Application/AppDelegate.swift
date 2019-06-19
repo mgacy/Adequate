4f2ecd09de044676ea50a2c1da3abd18c994e860
mgacy <matt@mgacy.com>
2019-06-18 20:28:32 -0700

    Add app state change logging

--- Adequate/Application/AppDelegate.swift
+++ Adequate/Application/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     private var notificationServiceManager: NotificationServiceManager?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        log.debug("\(#function) - \(String(describing: launchOptions))")
         self.window = UIWindow(frame: UIScreen.main.bounds)
 
         UNUserNotificationCenter.current().delegate = self
@@ -36,11 +37,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        log.verbose("WILL_RESIGN_ACTIVE")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        log.verbose("DID_ENTER_BACKGROUND")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -49,10 +52,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        log.verbose("DID_BECOME_ACTIVE")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        log.debug("WILL_TERMINATE")
     }
 
     // MARK: - URL-Specified Resources
@@ -85,6 +90,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         // Called for silent notifications.
+        log.debug("\(#function) - \(userInfo)")
         appCoordinator.refreshDealInBackground(completion: completionHandler)
     }
 }
@@ -96,6 +102,7 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter,
                                 willPresent notification: UNNotification,
                                 withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        log.debug("\(#function)")
         // TODO: prepare for other `response.notification.request.content.categoryIdentifier`
         appCoordinator.refreshDeal(showLoading: true)
 
@@ -107,7 +114,7 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter,
                                 didReceive response: UNNotificationResponse,
                                 withCompletionHandler completionHandler: @escaping () -> Void) {
-
+        log.debug("\(#function) - \(response)")
         let userInfo = response.notification.request.content.userInfo
 
         // TODO: prepare for other `response.notification.request.content.categoryIdentifier`
