9d4633cd943d75bfccd3ce331a4d0e3bf954d6d3
mgacy <matt@mgacy.com>
2019-06-20 17:23:51 -0700

    Setup test for screenshots

--- AdequateUITests/AdequateUITests.swift
+++ AdequateUITests/AdequateUITests.swift
@@ -9,6 +9,7 @@
 import XCTest
 
 class AdequateUITests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -16,8 +17,13 @@ class AdequateUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
+        app = XCUIApplication()
+
+        // Setup fastlane snapshots
+        setupSnapshot(app)
+
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -29,6 +35,46 @@ class AdequateUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+        // TODO: wait for loading to finish?
+        snapshot("01Deal")
+
+        let elementsQuery = app.scrollViews.otherElements
+        let adequateDealviewNavigationBar = elementsQuery.navigationBars["Adequate.DealView"]
+
+        // Show Story
+        adequateDealviewNavigationBar.buttons["StoryNavBar"].tap()
+        snapshot("02Story")
+
+        // Back to Deal
+        elementsQuery.navigationBars["Story"].buttons["LeftChevronNavBar"].tap()
+
+        // Show History
+        adequateDealviewNavigationBar.buttons["HistoryNavBar"].tap()
+        snapshot("03History")
+
+        // Show Settings
+        elementsQuery.navigationBars["History"].buttons["SettingsNavBar"].tap()
+        snapshot("04Settings")
+
+        // Dismiss Settings
+        app.navigationBars["Settings"].buttons["Done"].tap()
+
+        // Show HistoryDetail
+        //elementsQuery.tables.staticTexts["5-Pack: GenTek 9H Ceramic Liquid Screen Protector"].tap()
+        //elementsQuery.tables.staticTexts["iJoy Mini Projector Set with Screen and Speakers"].tap()
+
+        // Show FullscreenImage
+        //elementsQuery.collectionViews.cells.children(matching: .other).element.tap()
+
+        // Dismiss FullscreenImage
+
+        // Dismiss HistoryDetail
+        //app.navigationBars["Adequate.HistoryDetailView"].buttons["CloseNavBar"].tap()
+
+        // ...
+
+        //app.swipeLeft() // This results in swiping on the paged image view
     }
 
 }
