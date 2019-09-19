de898fa60b419832da02a415cf2f69c5b1c69ee4
mgacy <matt@mgacy.com>
2019-09-18 21:27:05 -0700

    Remove init?(coder:) implementation

--- Adequate/Scenes/Common/DealContentView.swift
+++ Adequate/Scenes/Common/DealContentView.swift
@@ -115,8 +115,7 @@ class DealContentView: UIView {
     }
 
     required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        self.configure()
+        fatalError("init(coder:) has not been implemented")
     }
 
     // MARK: - Configuration
