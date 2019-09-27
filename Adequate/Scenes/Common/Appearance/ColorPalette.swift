34dd9fa1b66589a3c6767d89daa5889302d96bf6
mgacy <matt@mgacy.com>
2019-09-27 13:22:05 -0700

    Refactor Appearance

---
+++ Adequate/Scenes/Common/Appearance/ColorPalette.swift
@@ -0,0 +1,18 @@
+//
+//  ColorPalette.swift
+//  Adequate
+//
+//  Created by Mathew Gacy on 9/27/19.
+//  Copyright © 2019 Mathew Gacy. All rights reserved.
+//
+
+import UIKit
+
+public struct ColorPalette {
+
+    // FullscreenImageViewController
+    // closeButton
+    public static let darkGray = UIColor(red: 0.207, green: 0.207, blue: 0.207, alpha: 1.0)
+
+    private init() {}
+}
