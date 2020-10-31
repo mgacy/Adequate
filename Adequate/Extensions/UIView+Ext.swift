8f9cd1b52c4304767be6a1bb1499e57c679ddf07
mgacy <matt@mgacy.com>
2020-10-30 20:44:43 -0700

    Add ParallaxTitleView

--- Adequate/Extensions/UIView+Ext.swift
+++ Adequate/Extensions/UIView+Ext.swift
@@ -27,3 +27,47 @@ extension UIView {
         return frame.width - directionalLayoutMargins.leading - directionalLayoutMargins.trailing
     }
 }
+
+// MARK: - Helpers
+extension UIView {
+
+    /// Returns superview with class name `className` if one exists.
+    /// - Parameter className: Class name of superview.
+    /// - Returns: Superview with class name `className`. Returns `nil` if view does not have matching superview.
+    public func superview(withName className: String) -> UIView? {
+        guard let superview = superview else {
+            return nil
+        }
+        let stringFromClass = NSStringFromClass(superview.classForCoder)
+        if stringFromClass == className {
+            return superview
+        } else {
+            return superview.superview(withName: className)
+        }
+    }
+
+    /// Returns first subview with class name `className` if one exists.
+    /// - Parameter className: Class name of subview.
+    /// - Returns: Subview with class name `className`. Returns `nil` if view does not have matching subview.
+    public func subview(withName className: String) -> UIView? {
+        for subview in subviews {
+            let stringFromClass = NSStringFromClass(subview.classForCoder)
+            if stringFromClass == className {
+                return subview
+            }
+        }
+        return nil
+    }
+
+    /// Returns first layout guide with identifier containing `substring` if one exists.
+    /// - Parameter substring: Identifier of layout guide.
+    /// - Returns: Layout guide with identifier `substring`. Returns `nil` if view does not have matching layout guide.
+    public func layoutGuideWithIdentifier(containing substring: String) -> UILayoutGuide? {
+        for layoutGuide in layoutGuides {
+            if layoutGuide.identifier.contains(substring) {
+                return layoutGuide
+            }
+        }
+        return nil
+    }
+}
