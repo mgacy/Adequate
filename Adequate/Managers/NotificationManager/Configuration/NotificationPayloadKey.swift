eabfb53d62e8ac0438d9fe572720e94d3d19be82
mgacy <matt@mgacy.com>
2020-11-17 13:06:26 -0700

    Improve documentation

--- Adequate/Managers/NotificationManager/Configuration/NotificationPayloadKey.swift
+++ Adequate/Managers/NotificationManager/Configuration/NotificationPayloadKey.swift
@@ -10,14 +10,24 @@ import Foundation
 
 /// Keys used in notification payload
 enum NotificationPayloadKey {
+
+    /// Key for the id of the `Deal` to which the notification applies.
     static let dealID = "deal-id"
 
-    // New Deal
+    // MARK: - New Deal
+
+    /// Key for the URL of the `Deal` on meh.com.
     static let dealURL = "deal-url"
+
+    /// Key for the URL of the first image for the `Deal`.
     static let imageURL = "image-url"
 
-    // Deal Delta
+    // MARK: - Deal Delta
+
+    /// Key for the `DeltaType` carried by the notification.
     static let deltaType = "delta-type"
+
+    /// Key for the associated value of the `DeltaType` carried by the notification.
     static let deltaValue = "delta-value"
 
     /// Values allowed for `deltaType`
