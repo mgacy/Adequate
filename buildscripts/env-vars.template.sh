d08495c4149d7fa9de077db5388807c4301331f2
mgacy <matt@mgacy.com>
2020-12-02 13:11:30 -0700

    Minor updates

--- buildscripts/env-vars.template.sh
+++ buildscripts/env-vars.template.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
-# AWS
+
 # This stretches the meaning of "secret", but it lets us keep additional AWS config together
 # TODO: replace `awsconfiguration.json` with this
 export SERVICE_REGION="USWest2"
 case $ENV_NAME in
 	development)
+		# AWS
 		export PLATFORM_APPLICATION_ARN="arn:aws:sns:xx-xxxx-1:xxxxxxxxxxxx:app/APNS_SANDBOX/Adequate-Development" 
 		export TOPIC_ARN="arn:aws:sns:xx-xxxx-1:xxxxxxxxxxxx:adequate-sam-dev-DealNotificationTopic-XXXXXXXXXXXXX"
+		# SwiftyBeaver
+        export LOGGER_APP_ID="xxxxxx"
+        export LOGGER_APP_SECRET="xxxxxx"
+        export LOGGER_ENCRYPTION_KEY="xxxxxx"
 		;;
 	staging)
+		# AWS
 		export PLATFORM_APPLICATION_ARN="arn:aws:sns:xx-xxxx-1:xxxxxxxxxxxx:app/APNS/Adequate-Production"
 		export TOPIC_ARN="arn:aws:sns:xx-xxxx-1:xxxxxxxxxxxx:adequate-sam-master-DealNotificationTopic-XXXXXXXXXXXX"
+		# SwiftyBeaver
+        export LOGGER_APP_ID="xxxxxx"
+        export LOGGER_APP_SECRET="xxxxxx"
+        export LOGGER_ENCRYPTION_KEY="xxxxxx"
 		;;
 	production)
+		# AWS
 		export PLATFORM_APPLICATION_ARN="arn:aws:sns:xx-xxxx-1:xxxxxxxxxxxx:app/APNS/Adequate-Production"
 		export TOPIC_ARN="arn:aws:sns:xx-xxxx-1:xxxxxxxxxxxx:adequate-sam-master-DealNotificationTopic-XXXXXXXXXXXX"
+		# We don't log to SwiftyBeaver platform in production
 		;;
 	*)
 		echo "Error: unrecognized ENV_NAME: '${ENV_NAME}'" 1>&2
 		exit 1
 		;;
 esac
-# SwiftyBeaver
-export LOGGER_APP_ID="xxxxxx"
-export LOGGER_APP_SECRET="xxxxxx"
-export LOGGER_ENCRYPTION_KEY="xxxxxx"
