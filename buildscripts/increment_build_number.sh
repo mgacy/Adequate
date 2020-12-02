ce5da8d38d6797578c59282ad9993351fad8cc28
mgacy <matt@mgacy.com>
2020-12-02 12:58:47 -0700

    Incremnet all build numbers

--- buildscripts/increment_build_number.sh
+++ buildscripts/increment_build_number.sh
@@ -3,3 +3,6 @@
 buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${INFOPLIST_FILE}")
 buildNumber=$(($buildNumber + 1))
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${INFOPLIST_FILE}"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "$SRCROOT/NotificationService/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "$SRCROOT/TodayExtension/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "$SRCROOT/WidgetExtension/Info.plist"
